dfc2b5ff9024873e7f802299cf9aec2c6608f3a1
jferrer23 <jferrer@csumb.edu>
2017-04-18 15:20:19 -0700

    connected pop up photo class to search screen class

--- app/src/main/java/com/example/ciyguide/ciyguide/ShowPhoto.java
+++ app/src/main/java/com/example/ciyguide/ciyguide/ShowPhoto.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 
 /**
  * Created by jason on 4/18/2017.
@@ -15,6 +16,14 @@ public class ShowPhoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout);
+        setContentView(R.layout.layout_photo);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(.8*width), (int)(height*.8));
     }
 }
